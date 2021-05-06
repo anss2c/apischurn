64ea7dfdb9e5c8c7a255555260eb72cae1c981ec
LAPTOP-P2HL0DJT\anis <anisa.aizatin@gmail.com>
2021-05-06 14:46:29 +0700

    first commit

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ app = Flask(__name__)
 
 @app.route('/predict', methods=['GET'])
 def predict():
+    modelpredic = joblib.load("model.pkl")
+    model_columns = joblib.load("model_columns.pkl")
     dataTesting = pd.read_csv("data/test1.csv")
     dataTesting.area_code = dataTesting.area_code.map({'area_code_415':415,'area_code_408':408,'area_code_510':510})
     dataTesting = dataTesting.replace({'voice_mail_plan':{'yes':1,'no':0}})
@@ -37,10 +39,10 @@ def index():
 
 if __name__ == '__main__':
 
-    modelpredic = joblib.load("model.pkl") # Load "model.pkl"
-    print ('Model loaded')
-    model_columns = joblib.load("model_columns.pkl") # Load "model_columns.pkl"
-    print ('Model columns loaded')
+    #modelpredic = joblib.load("model.pkl") # Load "model.pkl"
+    #print ('Model loaded')
+    #model_columns = joblib.load("model_columns.pkl") # Load "model_columns.pkl"
+    #print ('Model columns loaded')
 
     app.run(debug=True)
     # app.run(threaded=True, port=5000)
